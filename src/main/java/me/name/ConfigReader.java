b1c9a4db628c10901253abe988fbeaf3a5f63b2c
Arun Muthu <50038558+APM246@users.noreply.github.com>
2020-01-06 18:01:38 +0800

    Added src/main/resources to access bot token

--- src/main/java/me/name/ConfigReader.java
+++ src/main/java/me/name/ConfigReader.java
@@ -1,8 +1,6 @@
 package me.name;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.Properties;
 
 public class ConfigReader
@@ -10,10 +8,8 @@ public class ConfigReader
     public static String retrieveBotToken() throws IOException
     {
         Properties properties = new Properties();
-        String filename = "bot_token.config";
-        FileInputStream inputStream = new FileInputStream(filename);
+        InputStream inputStream = ConfigReader.class.getClassLoader().getResourceAsStream("bot_token.config");
         properties.load(inputStream);
-        System.out.println(properties.getProperty("bot_token"));
         return properties.getProperty("bot_token");
     }
 
